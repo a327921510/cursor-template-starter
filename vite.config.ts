d9a7f65dfc208bd7e509074a9fd20ef0fbf848bc
luoqing@jxxhy.cn <luoqing@jxxhy.cn>
2026-04-14 11:00:50 +0800

    [CLI] 补充tailwindcss 配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
+    tailwindcss(),
     svgr({
       svgrOptions: {
         icon: true,
