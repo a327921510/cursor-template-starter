c842bce43a8a620c00df6e44c4fd8dd544251c98
luoqing@jxxhy.cn <luoqing@jxxhy.cn>
2026-04-14 11:23:30 +0800

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -41,7 +41,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     port: 3000,
     proxy: {
-      "/api": {
+      "/api/": {
         target: "http://localhost:8080",
         changeOrigin: true,
       },
